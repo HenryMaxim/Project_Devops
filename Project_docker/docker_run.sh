b64744bfea20b416ab4fdcdf6fc56ebcbfae81a3
HenryMaxim <71707331+HenryMaxim@users.noreply.github.com>
2023-01-22 17:28:16 +0000

    Changed docker files

---
+++ Project_docker/docker_run.sh
@@ -0,0 +1,5 @@
+#docker build -t project_app: latest .
+#docker run -p 5151:5151 --name project_app project_app
+#docker ps
+#docker stop project_app
+docker rm project_app
--- Project_docker/main.py
+++ Project_docker/main.py
@@ -1,60 +1,71 @@
-from pydoc import plain
+from urllib import response
 from flask import Flask
 from flask import render_template
 import datetime
 import json
 import random
 import time
+import requests
 
 microweb_app = Flask(__name__)
 
-# List of provinces in Germany
+# Lijst van Duitse Provincies
 provinces = ["Baden-Württemberg", "Bayern", "Berlin", "Brandenburg", "Bremen", "Hamburg", "Hessen", "Mecklenburg-Vorpommern", "Niedersachsen", "Nordrhein-Westfalen", "Rheinland-Pfalz", "Saarland", "Sachsen", "Sachsen-Anhalt", "Schleswig-Holstein", "Thüringen"]
 
-# List of songs
+# Lijst van liedjes
 songs = ["Your Grounds", "Relaxing Moment", "Captain Coda", "Gypsy Shit", "New Wonders", "Soul Mate"]
 
 
 
-# Set to store used phone numbers
+# gebruikte telefoonnummers setten
 used_phone_numbers = set()
 
 def generate_random_data(num_votes):
-    # Create an empty list to store the data
+    # een lege lijst aanmaken voor al de data
     data_list = []
 
-    # Create a dictionary to store the count of votes for each song
+    # Aanmaken van lijst voor de stemmen per lied bij te houden
     song_votes = {}
+
+    # Aanmaken van lijst voor de stemmen per provincie bij te houden
+    province_song_votes = {}
     
-    # Initialize the vote count for each song to 0
+    # De vote counter op 0 zetten
     for song in songs:
         song_votes[song] = 0
 
     # Generate the specified number of votes
     for i in range(num_votes):
-        # Generate random phone number in the format "0XXX XX XX XX"
+        # Een random nummer genereren in het Duitse formaat "+49XXX XX XX XX"
         phone_number = f"+49{random.randint(100, 999)} {random.randint(10, 99)} {random.randint(10, 99)} {random.randint(10, 99)}"
 
-        # Keep generating a new phone number until we get one that has not been used
+        # Een telefoonnummer blijven genereren totdat er een ongebruikte bestaat
         while phone_number in used_phone_numbers:
             phone_number = f"+49{random.randint(100, 999)} {random.randint(10, 99)} {random.randint(10, 99)} {random.randint(10, 99)}"
 
-        # Add the generated phone number to the set of used phone numbers
+        # Het gemaakte telefoonnummer bij de gebruikte toevoegen zodat deze niet tweemaal gebruikt wordt
         used_phone_numbers.add(phone_number)
 
-        # Select a random province from the list
+        # Een random provincie van de lijst kiezen
         province = random.choice(provinces)
 
-        # Get current timestamp (precise to the second)
+        # De tijd tot op de seconde precies bepalen
         timestamp = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
 
-        # Select a random song from the list
+        # Een random lied van de lijst kiezen
         song = random.choice(songs)
 
-        # Increment the vote count for the selected song
+        # Bijtellen votes per song
         song_votes[song] += 1
 
-        # Create a dictionary with the generated data
+        #Bijtellen van votes per regio
+        if province not in province_song_votes:
+            province_song_votes[province]={}
+        if song not in province_song_votes[province]:
+            province_song_votes[province][song] = 0
+        province_song_votes[province][song] +=1
+
+        # de aangemaakte data samenvoegen
         data = {
             "timestamp": timestamp,
             "phone_number": phone_number,
@@ -62,20 +73,41 @@ def generate_random_data(num_votes):
             "song": song
         }
 
-        # Append the data dictionary to the list
+        # Toevoegen van de data aan de lijst
         data_list.append(data)
 
         song_votes = dict(sorted(song_votes.items(), key=lambda item: item[1], reverse=True))
 
         most_voted_song = max(song_votes, key=song_votes.get)
 
-    return data_list, song_votes, most_voted_song
+        most_voted_province_song = {}
+        for province in province_song_votes:
+            most_voted_province_song[province] = max(province_song_votes[province], key=province_song_votes[province].get)
+
+    return data_list, song_votes, most_voted_song, most_voted_province_song
 
 @microweb_app.route("/")
 def main():
     num_votes = 3000
-    data_list, song_votes, most_voted_song = generate_random_data(num_votes)
-    return render_template("index.html", datetime_now=datetime.datetime.now(), data_list=data_list, song_votes=song_votes, most_voted_song=most_voted_song)
+    data_list, song_votes, most_voted_song, most_voted_province_song = generate_random_data(num_votes)
+    return render_template("index.html", datetime_now=datetime.datetime.now(), data_list=data_list, song_votes=song_votes, most_voted_song=most_voted_song, most_voted_province_song=most_voted_province_song)
+    """
+    In veronderstelling dat post link online was
+    #converteren data naar json
+    data = json.dumps(data_list)
+    #api parameters instellen
+    url = "http://127.0.0.1:8080/districts/in?country=Germany"
+    headers ={"Content-Type": "application/json"}
+    #Posten van data
+    response = requests.post(url, data=data, headers=headers)
+    #printen van antwoord
+    print(response.status_code)
+    #nakijken van response
+    if response.status_code == 200:
+        print("Post succesvol")
+    else:
+        print("Post failed")
+    """
 
 if __name__ == "__main__":
     microweb_app.run(host="0.0.0.0", port=5151)
